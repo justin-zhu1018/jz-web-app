4edbed42e61211ec78d1b07a3b98a680a89781d9
unknown <justin.zhu@sjsu.edu>
2020-07-11 19:43:52 -0700

    heroku

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ app.use("/api", routes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "/build", "index.html"));
   });
 }
 
